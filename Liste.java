2ff5766081534d6791962b89de4142d6d7530b5c
Cr1st14n-Fr3sch1 <crifresh23@gmail.com>
2025-02-26 10:44:58 +0100

    Add files via upload

---
+++ Liste.java
@@ -0,0 +1,93 @@
+public class Liste {
+
+    private Nodo head;
+    private int Nelementi;
+
+    public Liste() {
+        head = null;
+        Nelementi = 0;
+    }
+
+    public void VisualizzaLista() {
+        Nodo nodo = head;
+
+        while (nodo != null) {
+            System.out.println(nodo.getDati());
+            nodo = nodo.getLink();
+        }
+    }
+
+    public void InserimentoInTesta(Object oggetto) {
+        Nodo nodo = new Nodo(oggetto);
+        nodo.setLink(head);
+        head = nodo;
+        Nelementi++;
+    }
+
+    public void InserimentoInCoda(Object oggetto) {
+        if (head != null) {
+            Nodo nodoAggiunto = new Nodo(oggetto);
+            Nodo nodoCiclo = head;
+
+            while (nodoCiclo.getLink() != null) {
+                nodoCiclo = nodoCiclo.getLink();
+            }
+
+            nodoCiclo.setLink(nodoAggiunto);
+            nodoAggiunto.setLink(null);
+        } else {
+            Nodo nodo = new Nodo(oggetto);
+            head = nodo;
+            Nelementi++;
+        }
+    }
+
+    public void EliminaTesta() {
+        if (head == null) {
+            return;
+        }
+
+        head = head.getLink();
+        Nelementi--;
+    }
+
+    public void EliminaCoda() {
+        if (head == null || head.getLink() == null) {
+            EliminaTesta();
+            return;
+        }
+
+        Nodo penultimo = head;
+        Nodo ultimo = head.getLink();
+
+        while (ultimo.getLink() != null) {
+            penultimo = ultimo;
+            ultimo = ultimo.getLink();
+        }
+
+        penultimo.setLink(null);
+        Nelementi--;
+    }
+
+    private class Nodo {
+        private Object dati;
+        private Nodo link;
+
+        public Nodo(Object dati) {
+            this.dati = dati;
+            this.link = null;
+        }
+
+        public Object getDati() {
+            return dati;
+        }
+
+        public Nodo getLink() {
+            return link;
+        }
+
+        public void setLink(Nodo link) {
+            this.link = link;
+        }
+    }
+}
